dc8927c131dabbfa91b8562ae09415bbed9340a7
张治政 <zhang15036896990@163.com>
2021-03-14 21:43:09 +0800

    ok

---
+++ Demo8/src/DServlet.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DServlet extends HttpServlet {
+
+	/**
+		 * Constructor of the object.
+		 */
+	public DServlet() {
+		super();
+	}
+
+	/**
+		 * Destruction of the servlet. <br>
+		 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+		 * The doGet method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to get.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String path=this.getServletContext().getRealPath("/MyHtml.html");//获取路径
+		System.out.println("path");
+		//获取目录下的文件
+		Set<String> pathsSet =this.getServletContext().getResourcePaths("/WEB-INF");
+		System.out.println(pathsSet);
+		//访问量统计
+	}
+ 
+	/**
+		 * Initialization of the servlet. <br>
+		 *
+		 * @throws ServletException if an error occurs
+		 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
