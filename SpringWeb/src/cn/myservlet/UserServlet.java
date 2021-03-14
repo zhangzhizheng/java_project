bbebf49ed60ccb6b333a22d49179658c35437072
张治政 <zhang15036896990@163.com>
2021-03-14 21:37:56 +0800

    ok

---
+++ SpringWeb/src/cn/myservlet/UserServlet.java
@@ -0,0 +1,57 @@
+package cn.myservlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import cn.myservice.UserService;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
+		/**
+		 * 因为Spring容器已经交由web容器初始化和管理
+                               获得WebApplicationContext对象，需要依赖ServletContext对象 
+	   	      通常在Servlet中完成
+		 */
+		//获得WebApplicationContext对象
+		WebApplicationContext webApplicationContext=WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		UserService userService=(UserService) webApplicationContext.getBean("userService");
+		userService.sayHello();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
